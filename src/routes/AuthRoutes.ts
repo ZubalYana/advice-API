0a4b2934aea3887bfaa6f090a39aec44367c0c68
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-11-07 06:03:32 +0200

    fix registration email bug

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -14,7 +14,7 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ message: "Name, email and password are required." });
         }
 
-        const existing = await User.findOne(email);
+        const existing = await User.findOne({ email });
         if (existing) return res.status(400).json({ message: "Email already exists" });
 
         const hashedPassword = await bcrypt.hash(password, 10);
